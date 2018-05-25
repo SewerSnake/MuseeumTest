04bce1e73d3dcaae78961cd2466cabfaf95e0ae7
SewerSnake <ericgroseclos@hotmail.com>
2018-05-25 17:17:53 +0200

    Can fetch from the web API

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var url = "https://www.rijksmuseum.nl/api/nl/collection?key=OTlO83oj&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614";
+
 addEventListener('load', function() {
-  alert('T');
+  getJSON();
 });
+
+function getJSON() {
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+      console.log(result);
+    });
+}
