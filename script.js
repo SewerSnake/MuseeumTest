7f46f12d5a10feb3aac0d35218f0b9aca2339439
Milja V <mmcvirtanen@gmail.com>
2018-05-30 15:02:15 +0200

    API queries

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var url = "https://www.rijksmuseum.nl/api/nl/collection?key=OTlO83oj&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614";
+var url = "https://www.rijksmuseum.nl/api/en/collection?key=OTlO83oj&format=json&q=vermeer&s=relevance";
+
+var collectionDetails = "https://www.rijksmuseum.nl/api/EN/collection/SK-A-2344?key=OTlO83oj&format=json"
+// https://www.rijksmuseum.nl/api/nl/collection?key=fakekey&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614
 
 var testCalender = "https://www.rijksmuseum.nl/api/nl/agenda/2018-05-28/expostition/0ee170d3-9604-48ac-b15f-014d911bf065/availability/e2b108b3-52b0-4a89-ac64-19514f8c5434?key=OTlO83oj&format=json";
 
@@ -7,15 +10,17 @@ addEventListener('load', function() {
 });
 
 function getJSON() {
-  fetch(url)
+  fetch(collectionDetails)
     .then(function(response) {
       return response.json();
     })
     .then(function(result) {
-      //console.log(result);
+      console.log(result);
       var artObjects = result.artObjects;
+      /*
       for (var i = 0; i < artObjects.length; i++) {
         console.log(artObjects[i].title);
       }
+      */
     });
 }
