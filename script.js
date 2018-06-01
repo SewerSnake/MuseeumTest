0c59e434255440eab395a3e5082f5ad19922589a
SewerSnake <ericgroseclos@hotmail.com>
2018-06-01 13:06:08 +0200

    removed unnecessary file and import. Managed to make two fetches from the web API in succession. Tested adding some dummy text to .content-wrap

--- script.js
+++ script.js
@@ -5,22 +5,39 @@ var collectionDetails = "https://www.rijksmuseum.nl/api/EN/collection/SK-A-2344?
 
 var testCalender = "https://www.rijksmuseum.nl/api/nl/agenda/2018-05-28/expostition/0ee170d3-9604-48ac-b15f-014d911bf065/availability/e2b108b3-52b0-4a89-ac64-19514f8c5434?key=OTlO83oj&format=json";
 
-addEventListener('load', function() {
+/*addEventListener('load', function() {
   getJSON();
-});
+});*/
 
-function getJSON() {
-  fetch(collectionDetails)
+function getArtist(artistName) {
+
+  fetch("https://www.rijksmuseum.nl/api/en/collection?key=OTlO83oj&format=json&q=" + artistName + "&s=relevance")
     .then(function(response) {
       return response.json();
     })
     .then(function(result) {
       console.log(result);
+
       var artObjects = result.artObjects;
+
+      var id = artObjects[0].objectNumber;
+
+      getPiece(id)
       /*
       for (var i = 0; i < artObjects.length; i++) {
         console.log(artObjects[i].title);
       }
       */
+    });
+}
+
+function getPiece(id) {
+  fetch("https://www.rijksmuseum.nl/api/EN/collection/" + id + "?key=OTlO83oj&format=json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+      console.log(result);
+
     });
 }
