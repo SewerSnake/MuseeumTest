cfe9c3987e3c131abfacd0eedaaa200ba897ff2e
Milja V <mmcvirtanen@gmail.com>
2018-06-06 12:40:50 +0200

    Solving merge conflict.

--- index.jsx
+++ index.jsx
@@ -54,7 +54,7 @@ var Square = require('./components/square.jsx');
     render() {
         return <section id="section-a">
                   <div className="content-wrap">
-                    <h2 className="content-title">Art & web technology</h2>
+                    <h2 className="content-title">Fine art & web technology</h2>
                     <p>Built with:</p>
                       <div id="builtWithCards">
                         <Card color="lightgrey" colorL="#FFC983" name="React" image={'media/React-icon.svg'}/>
@@ -128,18 +128,6 @@ class Card extends React.Component {
     componentDidMount() {
       var artistName = this.props.match.params.artistName;
       var artArr = [];
-
-      if (artistName == 'AdriaenCoorte') {
-        artistName = 'coorte';
-      }
-
-      if (artistName == 'WillemClaesz') {
-        artistName = 'willem&nbsp;claesz';
-      }
-
-      if (artistName == 'FlorisClaesz') {
-        artistName = 'floris&nbsp;claesz';
-      }
 
       fetch("https://www.rijksmuseum.nl/api/en/collection?key=OTlO83oj&format=json&q="
       + artistName + "&s=relevance" + "&ps=" + 5)
@@ -308,7 +296,6 @@ class Card extends React.Component {
         }
       );
     }
-
     onChange(event){
       var momentt = moment(event.target.value).format('YYYY-MM-DD');
       this.setState({ calendarDate: momentt });
@@ -323,15 +310,11 @@ class Card extends React.Component {
         }
       );
     }
-
     render(){
-      // moment.tz.add('Europe/Amsterdam|AMT NST +0120 +0020 CEST CET|-j.w -1j.w -1k -k -20 -10|010101010101010101010101010101010101010101012323234545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545454545|-2aFcj.w 11b0 1iP0 11A0 1io0 1cM0 1fA0 1a00 1fA0 1a00 1fA0 1a00 1co0 1io0 1yo0 Pc0 1a00 1fA0 1Bc0 Mo0 1tc0 Uo0 1tA0 U00 1uo0 W00 1s00 VA0 1so0 Vc0 1sM0 UM0 1wo0 Rc0 1u00 Wo0 1rA0 W00 1s00 VA0 1sM0 UM0 1w00 fV0 BCX.w 1tA0 U00 1u00 Wo0 1sm0 601k WM0 1fA0 1cM0 1cM0 1cM0 16M0 1gMM0 1a00 1fA0 1cM0 1cM0 1cM0 1fA0 1a00 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|16e5');
-
       //Kan flyttas till CSS senare
       var border = {padding: 1, border: 1, borderStyle: 'dotted'};
 
       var eventsToDisplay = this.state.allEvents.map((event, index) => {
-      //  var s = moment(event.period.startDate);
         var available = <a href={event.links.web}>Book</a>;
 
           moment().calendar((event.period.startDate), {
@@ -345,18 +328,15 @@ class Card extends React.Component {
               }
             }
           });
-
-      //  var timeLeft = moment().diff(event.period.startDate, 'days');
-        console.log('timeleft: ' + moment(event.period.startDate).diff(moment(), 'days'));
         return <tr key={index}><td>{event.period.text}</td><td>{event.exposition.name}</td><td>{available}</td></tr>;
       });
 
-      return <div id="plannerTable">
-      <form>search date:<br/><input type="date" min={this.state.today} onChange={this.onChange}/></form>
+      return <div >
+      <form><input type="date" min={this.state.today} onChange={this.onChange}/></form>
       <br />
-      <table>
+      <table style={border}>
       <tbody>
-      <tr><th id="thDate" >{this.state.calendarDate}</th></tr>
+      <tr><th>{this.state.calendarDate}</th></tr>
       <tr>
         <th>Time</th>
         <th>Event</th>
@@ -369,7 +349,110 @@ class Card extends React.Component {
     }
   }
 
+  //MILJA KOD >>>
+  class Menu extends React.Component{
+    constructor(){
+      super();
+      this.state = {
+        menuArray: []
+      }
+    }
+    componentDidMount(){
+      console.log('mounted');
+      //postMenu()
 
+      fetch('http://cities.jonkri.se/', {
+        body: JSON.stringify({cafeMenu, id: MENU_ID}),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }).then(response => response.json())
+        .then(result => {
+          console.log('got result');
+          fetch('http://cities.jonkri.se/'+MENU_ID)
+          .then(response => response.json())
+          .then(result => {
+            return result
+          })
+          .then(result => {
+            console.log('got array');
+            this.setState({ menuArray: result });
+          });
+        })
+    }
+    render(){
+      console.log('render');
+      return <div><h1>HALLOHALLOHALLOOOOO!!!</h1></div>
+    }
+  }
+
+  var MENU_ID = 'fullmenu';
+  var cafeMenu =
+  [
+    {
+      name: "Espresso",
+      population: 0,
+      price: 2.10,
+      sugar: false
+    },
+    {
+      name: "Drip Coffee",
+      population: 0,
+      price: 2.20,
+      sugar: false
+    },
+    {
+      name: "Cold Brew",
+      population: 0,
+      price: 3.00,
+      sugar: false
+    },
+    {
+      name: "Ice Tea",
+      population: 0,
+      price: 2.95,
+      sugar: false
+    },
+    {
+      name: "Hot Tea",
+      population: 0,
+      price: 2.95,
+      sugar: false
+    },
+    {
+      name: "Cappuccino",
+      population: 0,
+      price: 2.85,
+      sugar: false
+    },
+    {
+      name: "Latte",
+      population: 0,
+      price: 2.95,
+      sugar: false
+    },
+    {
+      name: "Americano",
+      population: 0,
+      price: 2.40,
+      sugar: false
+    }
+  ];
+    // function updateOrder(){
+    //   fetch('http://cities.jonkri.se/'+MENU_ID, {
+    //     body: JSON.stringify({ EDITEDMENU }),
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     method: 'PUT'
+    //   }).then(response => response.json())
+    //     .then(result => {
+    //       console.log(result);
+    //     });
+    // }
+
+  //<<< MILJA KOD SLUT
 
   var HashRouter = ReactRouterDOM.HashRouter; var Link = ReactRouterDOM.Link; var Route = ReactRouterDOM.Route; ReactDOM.render(
 
@@ -395,7 +478,7 @@ class Card extends React.Component {
       <Route exact={true} component={Art} path="/art"   />
       <Route component={ArtGallery} path="/art/:artistName" />
       <Route exact={true} component={Planner} path="/planner" />
-      <Route path="/cafe" />
+      <Route component={Menu} path="/cafe" />
     </div>
   </HashRouter>, document.getElementById('box2'));
 
@@ -452,93 +535,3 @@ class Card extends React.Component {
       );
     }
   }
-
-  var MENU_ID = 111;
-  var cafeMenu =
-  [
-    {
-      name: "Espresso",
-      population: 0,
-      price: 2.10,
-      sugar: false
-    },
-    {
-      name: "Drip Coffee",
-      population: 0,
-      price: 2.20,
-      sugar: false
-    },
-    {
-      name: "Cold Brew",
-      population: 0,
-      price: 3.00,
-      sugar: false
-    },
-    {
-      name: "Ice Tea",
-      population: 0,
-      price: 2.95,
-      sugar: false
-    },
-    {
-      name: "Hot Tea",
-      population: 0,
-      price: 2.95,
-      sugar: false
-    },
-    {
-      name: "Cappuccino",
-      population: 0,
-      price: 2.85,
-      sugar: false
-    },
-    {
-      name: "Latte",
-      population: 0,
-      price: 2.95,
-      sugar: false
-    },
-    {
-      name: "Americano",
-      population: 0,
-      price: 2.40,
-      sugar: false
-    }
-  ];
-
-  //POST request + create menu items
-  function postMenu(){
-    fetch('http://cities.jonkri.se/', {
-      body: JSON.stringify({cafeMenu, id: MENU_ID}),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    }).then(response => response.json())
-      .then(result => {
-        console.log(result);
-/*
-        var renderCards = cafeMenu.map(function (value, index) {
-          var img = value.name;
-          return <MenuCard id={index} color="lightgrey" colorL="#FFC983" name={value.name} image={img}/>
-        })
-        ReactDOM.render(<section id="cafeMenu">
-          <h1>MENU</h1>
-          <div>{renderCards}</div>
-        </section>, document.getElementById('box2'));
-*/
-      });
-  }
-
-  // function updateOrder(){
-  //   fetch('http://cities.jonkri.se/'+MENU_ID, {
-  //     body: JSON.stringify({ EDITEDMENU }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     method: 'PUT'
-  //   }).then(response => response.json())
-  //     .then(result => {
-  //       console.log(result);
-  //     });
-  // }
