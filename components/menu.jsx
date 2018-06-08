2b882c1b5f8d6445ad9b5c7292e1f7ca76c8a9ab
Milja V <mmcvirtanen@gmail.com>
2018-06-08 10:06:32 +0200

    Working on Redux

--- components/menu.jsx
+++ components/menu.jsx
@@ -1,124 +1,210 @@
 var React = require('react')
+var ReactDOM = require('react-dom');
+var { Provider } = require('react-redux');
 var MenuCard = require('./menucard.jsx');
+var Redux = require('redux');
+var ReactRedux = require('react-redux');
 
-class Menu extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      menuArray: []
-    }
-    this.handleClick = this.handleClick.bind(this);
+// <<<<<< REDUX TEST
+
+var MENU_ID = 'fullmenu';
+
+class Menu extends React.Component {
+  render() {
+    console.log('render');
+    console.log(this.props.menu);
+    return <div><h1>...</h1></div>;
   }
-  componentDidMount(){
-    console.log('mounted');
-/*
-    fetch('http://cities.jonkri.se/', {
-      body: JSON.stringify({cafeMenu, id: MENU_ID}),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    }).then(response => response.json())
-      .then(result => {
-        console.log('got result');
-*/
-        fetch('http://cities.jonkri.se/'+MENU_ID)
-        .then(response => response.json())
-        .then(result => {
-          return result
+}
+
+var ConnectedMenu = ReactRedux.connect(
+  function(state) {
+    return { menu: state.menu };
+  },
+  function(dispatch){
+    return {
+      addMenu: function() {
+        console.log('button');
+        return dispatch({
+          type: 'SET_MENU'
         })
-        .then(result => {
-          this.setState({ menuArray: result.cafeMenu });
-        });
-//      })
-  }
-  handleClick(){
-    console.log('Clicked!');
-    console.log(this.state.menuArray);
-    var finalOrder = this.state.menuArray;
+      }
+    };
+  })(Menu);
 
-    //TODO: Use PUT to post order menu to API. (then fetch menu and render a final order div?)
-    fetch('http://cities.jonkri.se/'+MENU_ID, {
-        body: JSON.stringify({ finalOrder, id: MENU_ID }),
+//REDUCER
+var reducer = function(state, action) {
+  console.log('action: ', action.payload);
+  switch (action.type) {
+    case 'SET_MENU':
+      console.log('setmenu');
+      return {
+        state: action.payload
+      };
+    default: return state;
+  }
+};
+
+//STATE: menu: { drink: {...}, {...} }
+var store = Redux.createStore(reducer, { menu: {}, id: MENU_ID});
+
+// API ANROP - NOLLSTÄLLER MENYN
+fetch('http://cities.jonkri.se')
+  .then(response => response.json())
+  .then(result => {
+    var array = result;
+    let object = array.find(o => o.id === MENU_ID);
+    if (object === undefined) {
+      console.log('Menu not found, POSTing')
+      fetch('http://cities.jonkri.se/', {
+        body: JSON.stringify({cafeMenu, id: MENU_ID}),
         headers: {
           'Content-Type': 'application/json'
         },
-        method: 'PUT'
+        method: 'POST'
       }).then(response => response.json())
         .then(result => {
-          console.log('final order:');
-          console.log(result);
-        });
+        console.log('POST result', result);
+        var object = result.find(o => o.id === MENU_ID);
+        console.log('object:', object);
+        store.dispatch({ type: 'SET_MENU', payload: object });
+      })
+    }  else {
+      console.log('Found old menu, resetting');
+      // DELETE EDITED MENU
+      // fetch('http://cities.jonkri.se/'+MENU_ID, {
+      //   method: 'DELETE'
+      // })
+      fetch('http://cities.jonkri.se/' + MENU_ID, {
+        body: JSON.stringify({cafeMenu, id: MENU_ID}),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'PUT'})
+      .then(response => response.json()).then(result => {
+        console.log('PUT result', result); //Get ett {menu:, id: }
+        store.dispatch({ type: 'SET_MENU', payload: result})
+      })
+    }
+  })
 
-  }
-  render(){
-    console.log('render');
-    var menuItems = this.state.menuArray.map((item, index) => {
-      return <MenuCard key={index} color="lightgrey" colorL="#FFFFFF" item={item} name={item.name} image={index} />
-    });
-    console.log(this.state.menuArray);
-    return <div>{menuItems}<br/><br/><button type="button" onClick={this.handleClick}>Place order</button></div>
-  }
-}
-var MENU_ID = 'fullmenu';
+ReactDOM.render(<Provider store={store}>
+<div><ConnectedMenu/ ></div>
+</Provider>, document.getElementById('box2'));
+
+// >>>>>> REDUX END TEST
+
+
+// class Menu extends React.Component{
+//   constructor(){
+//     super();
+//     this.state = {
+//       menuArray: []
+//     }
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//   componentDidMount(){
+//     console.log('mounted');
+//
+//     // fetch('http://cities.jonkri.se/', {
+//     //   body: JSON.stringify({cafeMenu, id: MENU_ID}),
+//     //   headers: {
+//     //     'Content-Type': 'application/json'
+//     //   },
+//     //   method: 'POST'
+//     // }).then(response => response.json())
+//     //   .then(result => {
+//     //     console.log('got result');
+//
+//         fetch('http://cities.jonkri.se/'+MENU_ID)
+//         .then(response => response.json())
+//         .then(result => {
+//           return result
+//         })
+//         .then(result => {
+//           this.setState({ menuArray: result.cafeMenu });
+//         });
+//     //  })
+//   }
+//   handleClick(){
+//     console.log('Clicked!');
+//     console.log(this.state.menuArray);
+//     var finalOrder = this.state.menuArray;
+//
+//     //TODO: Use PUT to post order menu to API. (then fetch menu and render a final order div?)
+//     fetch('http://cities.jonkri.se/'+MENU_ID, {
+//         body: JSON.stringify({ finalOrder, id: MENU_ID }),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         method: 'PUT'
+//       }).then(response => response.json())
+//         .then(result => {
+//           console.log('final order:');
+//           console.log(result);
+//         });
+//
+//   }
+//   render(){
+//     console.log('render');
+//     var menuItems = this.state.menuArray.map((item, index) => {
+//       return <MenuCard key={index} color="lightgrey" colorL="#FFFFFF" item={item} name={item.name} image={index} />
+//     });
+//     console.log(this.state.menuArray);
+//     return <div>{menuItems}<br/><br/><button type="button" onClick={this.handleClick}>Place order</button></div>
+//   }
+// }
+
 var cafeMenu =
-[
-  {
+{
+  espresso: {
     name: "Espresso",
-    population: 0,
     price: 2.10,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  dripcoffee: {
     name: "Drip Coffee",
-    population: 0,
     price: 2.20,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  coldbrew: {
     name: "Cold Brew",
-    population: 0,
     price: 3.00,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  icetea: {
     name: "Ice Tea",
-    population: 0,
     price: 2.95,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  hottea: {
     name: "Hot Tea",
-    population: 0,
     price: 2.95,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  cappuccino: {
     name: "Cappuccino",
-    population: 0,
     price: 2.85,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  latte: {
     name: "Latte",
-    population: 0,
     price: 2.95,
     sugar: false,
-    orders: 0
+    cups: 0
   },
-  {
+  americano: {
     name: "Americano",
-    population: 0,
     price: 2.40,
     sugar: false,
-    orders: 0
+    cups: 0
   }
-];
+};
 
 module.exports = Menu;
