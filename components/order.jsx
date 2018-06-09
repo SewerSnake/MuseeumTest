f9da67e9bda708ac1d1c8eb010d3a8c401250346
Milja V <mmcvirtanen@gmail.com>
2018-06-09 12:27:56 +0200

    Order table updates when drink chosen

--- components/order.jsx
+++ components/order.jsx
@@ -8,31 +8,26 @@ var ReactRedux = require('react-redux');
 
 class Order extends React.Component{
   // constructor() {
-  //   // super();
-  //   // this.state = {
-  //   //   customerOrder: ''
-  //   // }
-  //   // this.componentDidMount = this.componentDidMount.bind(this);
-  //   // this.componentDidUpdate = this.componentDidUpdate.bind(this);
+  //   super();
+  //   this.state = {
+  //     orderedItems: []
+  //   }
+  //   this.componentDidMount = this.componentDidMount.bind(this);
+  //   this.componentDidUpdate = this.componentDidUpdate.bind(this);
   // }
   componentDidMount() {
     console.log('component did mount, Order');
-    // // console.log('Order.jsx: this.props.menu', this.props.menu);
-    // this.setState = ({order: 'didmount'});
-    // console.log('customerOrder ', this.state.customerOrder);
   }
   componentDidUpdate() {
     console.log('component did update');
-    var values = Object.values(this.props.menu);
-    console.log('update, values', Object.values(this.props.menu));
-    var drinksOrdered = values.filter((drink) => drink.cups > 0);
-    console.log(drinksOrdered);
   }
   render() {
+    var drinksOrdered = Object.values(this.props.menu).filter((drink) => drink.cups > 0);
+
     return <table>
       <tbody>
         <tr><th>Drink</th><th>Sugar</th><th>Amount</th></tr>
-        <td>Drip Coffee</td>
+        <td>{drinksOrdered.map(drink => <tr key={drink.name}>{drink.name}</tr>)}</td>
         <td>Sugar yes/no</td>
         <td>2</td>
       </tbody>
