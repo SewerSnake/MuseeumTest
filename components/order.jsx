6e9e92ce151e9e85c586ebdb5102d9f0ff666a77
Milja V <mmcvirtanen@gmail.com>
2018-06-09 12:18:47 +0200

    Ordered drinks filtered correctly in console

--- components/order.jsx
+++ components/order.jsx
@@ -7,34 +7,33 @@ var ReactRedux = require('react-redux');
 
 
 class Order extends React.Component{
-  constructor() {
-    super();
-    this.state = {
-      customerOrder: ''
-    }
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
-  }
+  // constructor() {
+  //   // super();
+  //   // this.state = {
+  //   //   customerOrder: ''
+  //   // }
+  //   // this.componentDidMount = this.componentDidMount.bind(this);
+  //   // this.componentDidUpdate = this.componentDidUpdate.bind(this);
+  // }
   componentDidMount() {
     console.log('component did mount, Order');
-    // console.log('Order.jsx: this.props.menu', this.props.menu);
-    this.setState = ({order: 'didmount'});
-    console.log('customerOrder ', this.state.customerOrder);
+    // // console.log('Order.jsx: this.props.menu', this.props.menu);
+    // this.setState = ({order: 'didmount'});
+    // console.log('customerOrder ', this.state.customerOrder);
   }
   componentDidUpdate() {
     console.log('component did update');
     var values = Object.values(this.props.menu);
     console.log('update, values', Object.values(this.props.menu));
-    var drinksWithSugar = values.filter((drink) => drink.sugar);
-    console.log(drinksWithSugar);
-    this.setState = ({ customerOrder: 'sugar'});
+    var drinksOrdered = values.filter((drink) => drink.cups > 0);
+    console.log(drinksOrdered);
   }
   render() {
     return <table>
       <tbody>
         <tr><th>Drink</th><th>Sugar</th><th>Amount</th></tr>
         <td>Drip Coffee</td>
-        <td>{this.state.customerOrder}</td>
+        <td>Sugar yes/no</td>
         <td>2</td>
       </tbody>
     </table>
